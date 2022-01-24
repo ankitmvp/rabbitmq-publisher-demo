7c5f1b7e21348951a32571d65721ca32bc43d13c
Ankit Bansal <ankitbansalniit@outlook.com>
2022-01-24 10:53:44 +0530

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client.Core.DependencyInjection.Services;
+
+namespace rabbitmq_demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IQueueService RmqService;
+        public WeatherForecastController(IQueueService rmqService)
+        {
+            RmqService = rmqService;
+        }
+
+        [HttpPost]
+        [Route("PublishMessage")]
+        public async Task<IActionResult> PublishMessage([FromQuery] string message)
+        {
+            await RmqService.SendAsync(@object: message, exchangeName: "demoexchange", routingKey: "demokey");
+            return Ok("Message publised.");
+        }
+    }
+}
